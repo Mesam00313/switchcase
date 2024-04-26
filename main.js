6a757ac3caff7dba7487a5750f7b3b206b116005
mesam_ali <159754333+Mesam00313@users.noreply.github.com>
2024-04-27 00:33:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+#! /Usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//step 1
+const answers = await inquirer.prompt([
+    { type: "number",
+        name: "numberone",
+        message: (chalk.yellowBright("enter your first number")) },
+    {
+        type: "number",
+        name: "numbertwo",
+        message: (chalk.redBright("enter your second number"))
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: (chalk.greenBright("choose your opertor")),
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+//step 2
+const { numberone, numbertwo, operator } = answers;
+let result;
+switch (operator) {
+    case "+":
+        result = numberone + numbertwo;
+        break;
+    case "-":
+        result = numberone - numbertwo;
+        break;
+    case "*":
+        result = numberone * numbertwo;
+        break;
+    case "/":
+        result = numberone / numbertwo;
+        break;
+    default:
+        console.log("invalid operator");
+        break;
+}
+console.log(chalk.red(`${numberone} ${operator} ${numbertwo} = ${result}`));
